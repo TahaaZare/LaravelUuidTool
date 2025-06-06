4416221131b747949f5a5de497b10eccd8aafc7c
TahaaZare <tahaazre.dev@gmail.com>
2025-06-06 23:52:44 +0330

    Add Length

--- src/UuidGenerator.php
+++ src/UuidGenerator.php
@@ -1,28 +1,46 @@
 <?php
 
 namespace Tahaazare\LaravelUuidTool;
+
 use Illuminate\Support\Str;
 
 class UuidGenerator
 {
     /**
      * @param string $type string OR int
+     * @param int|null $length
      * @return string
      */
-    public function generate($type = 'string')
+    public static function generate(string $type = 'string', ?int $length = null): string
     {
         return match ($type) {
             'string' => (string) Str::uuid(),
-            'int' => $this->generateNumeric(),
+            'int' => self::generateNumeric($length),
             default => throw new \InvalidArgumentException("The UUID type is invalid: $type"),
         };
     }
 
     /**
+     *
+     * @param int|null $length
      * @return string
      */
-    private function generateNumeric(): string
+    private static function generateNumeric(?int $length = null): string
     {
-        return (string) hexdec(str_replace('-', '', Str::uuid()));
+        $hex = str_replace('-', '', Str::uuid());
+
+        $decimal = hexdec(substr($hex, 0, 12));
+
+        $decimalStr = (string)$decimal;
+
+        if ($length === null) {
+            return $decimalStr;
+        }
+
+        if ($length <= strlen($decimalStr)) {
+            return substr($decimalStr, 0, $length);
+        }
+
+        return str_pad($decimalStr, $length, '0', STR_PAD_RIGHT);
     }
 }
