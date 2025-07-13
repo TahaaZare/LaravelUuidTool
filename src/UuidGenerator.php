412b36d5d26c33fd5ce4274c42fb79e2c4d55606
TahaaZare <tahaazre.dev@gmail.com>
2025-07-13 09:32:37 +0330

    :up: `generateFor`

--- src/UuidGenerator.php
+++ src/UuidGenerator.php
@@ -46,20 +46,28 @@ class UuidGenerator
     }
 
 
-    public function generateFor(Model|string $model, string $column = 'uuid', int $minLength = 3): string
+    public function generateFor(Model|string $model, string $column = 'uuid', string $type = 'string', int $minLength = 3): string
     {
         $length = $minLength;
 
         do {
-            $uuid = str_pad(mt_rand(0, pow(10, $length) - 1), $length, '0', STR_PAD_LEFT);
+            $uuid = match ($type) {
+                'string' => (string) Str::uuid(),
+                'int' => str_pad((string) mt_rand(0, pow(10, $length) - 1), $length, '0', STR_PAD_LEFT),
+                default => throw new \InvalidArgumentException("Invalid UUID type: $type"),
+            };
+
             $exists = $model::where($column, $uuid)->exists();
-            $length++;
+            if ($type === 'int') {
+                $length++;
+            }
         } while ($exists && $length <= 20);
 
         if ($exists) {
-            throw new \RuntimeException("Failed to generate unique UUID after maximum attempts.");
+            throw new \RuntimeException("Failed to generate unique $type UUID after maximum attempts.");
         }
 
         return $uuid;
     }
+
 }
